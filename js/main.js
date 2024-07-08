f784f97c55823f9a82a14fccd4ad481af518099f
Ronald <ronaldchingambare2@gmail.com>
2024-07-08 12:49:03 +0200

    Second Initial

--- js/main.js
+++ js/main.js
@@ -1,12 +1,43 @@
 $(document).ready(function () {
-    var navbar = $('#nav'); // Replace with your navbar class
-    var threshold = 50; // Adjust this value to control the scroll position for the change
 
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > threshold) {
-            navbar.css("background-color", "#d9ebf4"); // Replace with your desired color
-        } else {
-            navbar.css("background-color", "transparent"); // Replace with your navbar's original color
+    $('.faqs').fadeOut();
+    const hidefaqs = () => {
+        $('#faq01').fadeOut(500);
+        $('#faq02').fadeOut(500);
+        $('#faq03').fadeOut(500);
+        $('#faq04').fadeOut(500);
+        $('#faq05').fadeOut(500);
+        $('#faq06').fadeOut(500);
+    }
+
+    hidefaqs();
+    $('#faq01').fadeIn(500);
+    $('.faq').on('click', function () {
+        var id = $('this').attr('id');
+        $(id).addClass('bg-blue-50 border-blue-700');
+        hidefaqs();
+        switch (id) {
+            case 'faq-1':
+                $('#faq01').fadeIn(500);
+                break;
+            case 'faq-2':
+                $('#faq02').fadeIn(500);
+                break;
+            case 'faq-3':
+                $('#faq03').fadeIn(500);
+                break;
+            case 'faq-4':
+                $('#faq04').fadeIn(500);
+                break;
+            case 'faq-5':
+                $('#faq05').fadeIn(500);
+                break;
+            default:
+                $('#faq01').fadeIn(500);
+                break;
         }
+
+
+
     });
 });
