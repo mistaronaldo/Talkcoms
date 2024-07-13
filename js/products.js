acbd28ccdfe10262467436b937cdc0c0b3bb9fa3
Ronald <ronaldchingambare2@gmail.com>
2024-07-13 18:22:42 +0200

    Mobile

--- js/products.js
+++ js/products.js
@@ -8,13 +8,23 @@ $(document).ready(function () {
         var targetDiv = $('#landing-img-cont');
         imageToMove.appendTo(targetDiv);
 
+
+        var carouselMobile = $('#carousel-item-mobile');
+        var carouselItems = $('.carousel-item');
+        carouselItems.addClass('hidden duration-200 ease-linear');
+        carouselItems.attr('data-carousel-item', '');
+        console.log(carouselItems);
+
+        carouselMobile.append(carouselItems);
+
+
+
+        var toslide = $('.toslide');
+        toslide.attr('id', 'sliding');
+        toslide.addClass('keen-slider')
+        toslide.children().addClass('keen-slider__slide');
+        var newslider = new KeenSlider("#sliding");
     }
 
-    var carouselMobile = $('#carousel-item-mobile');
-    var carouselItems = $('.carousel-item');
-    carouselItems.addClass('hidden duration-200 ease-linear');
-    carouselItems.attr('data-carousel-item','');
-    console.log(carouselItems);
 
-    carouselMobile.append(carouselItems)
 })
