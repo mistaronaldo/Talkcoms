bd1642e74c47226c9faa33c8384f66e240896a7c
Ronald <ronaldchingambare2@gmail.com>
2024-07-14 10:00:49 +0200

    Mobile kinda done now

--- js/init-slier.js
+++ js/init-slier.js
@@ -23,36 +23,41 @@ function updateForm(index, slider) {
   // }
 }
 
+$(document).ready(function () {
+  var screenWidth = $(window).width();
+  if (screenWidth >= 640) {
 
-var slider = new KeenSlider("#my-keen-slider", {
-  breakpoints: {
-    "(min-width: 400px)": {
-      slides: { perView: 2, spacing: 5 },
-    },
-    "(min-width: 1000px)": {
-      slides: { perView: 3, spacing: 10 },
-    },
-  },
-  slides: { perView: 1 },
-});
+    var slider = new KeenSlider("#my-keen-slider", {
+      breakpoints: {
+        "(min-width: 400px)": {
+          slides: { perView: 2, spacing: 5 },
+        },
+        "(min-width: 1000px)": {
+          slides: { perView: 3, spacing: 10 },
+        },
+      },
+      slides: { perView: 1 },
+    });
 
-slider.on("slideChanged", () => {
-  var slide = slider.track.details.rel
-  updateForm(slide, slider)
-});
+    slider.on("slideChanged", () => {
+      var slide = slider.track.details.rel
+      updateForm(slide, slider)
+    });
 
-setInterval(function () {
-  var slide = slider.track.details.rel
-  if (slide === slider.track.details.slides.length - 3) {
-    slider.track.to(1)
-  } else {
+    setInterval(function () {
+      var slide = slider.track.details.rel
+      if (slide === slider.track.details.slides.length - 3) {
+        slider.track.to(1)
+      } else {
 
-    slider.next();
-    mobileslider.next();
+        slider.next();
+        mobileslider.next();
+      }
+
+    }, 10000)
+
+    slider.slides[0].classList.add('testimonial-left');
+    slider.slides[1].classList.add('testimonial-center');
+    slider.slides[2].classList.add('testimonial-right');
   }
-
-}, 10000)
-
-slider.slides[0].classList.add('testimonial-left');
-slider.slides[1].classList.add('testimonial-center');
-slider.slides[2].classList.add('testimonial-right');
+})
--- js/products.js
+++ js/products.js
@@ -23,7 +23,19 @@ $(document).ready(function () {
         toslide.attr('id', 'sliding');
         toslide.addClass('keen-slider')
         toslide.children().addClass('keen-slider__slide');
-        var newslider = new KeenSlider("#sliding");
+        var newslider1 = new KeenSlider("#sliding");
+
+        var toslide = $('.toslide2');
+        toslide.attr('id', 'sliding2');
+        toslide.addClass('keen-slider')
+        toslide.children().addClass('keen-slider__slide');
+        var newslider1 = new KeenSlider("#sliding2");
+
+        var toslide = $('.toslide3');
+        toslide.attr('id', 'sliding3');
+        toslide.addClass('keen-slider')
+        toslide.children().addClass('keen-slider__slide');
+        var newslider1 = new KeenSlider("#sliding3");
     }
 
 
