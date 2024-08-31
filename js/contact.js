3b2068c06c81cea7cfadda872c27119af08c6248
Ronald <ronaldchingambare2@gmail.com>
2024-08-31 18:09:44 +0200

    Add resouces page

--- js/contact.js
+++ js/contact.js
@@ -147,7 +147,7 @@ document.addEventListener("alpine:init", () => {
             const newSelect = document.createElement("div");
             newSelect.className = style.select;
             newSelect.setAttribute("x-bind", "selectTrigger");
-            // newSelect.classList.add('h-16 md:text-md font-light block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6')
+            // newSelect.classList.add('h-16 md:text-md font-light block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-indigo-600 sm:text-sm sm:leading-6')
 
             const textList = document.createElement("span");
             textList.className = style.textList;
